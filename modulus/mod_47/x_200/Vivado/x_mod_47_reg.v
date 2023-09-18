497b3f2c6b9cc62a33d76df2ae4c3ca096d953fd
ZeboZebo702 <84804601+ZeboZebo702@users.noreply.github.com>
2023-09-19 00:53:15 +0100

    Add files via upload

---
+++ modulus/mod_47/x_200/Vivado/x_mod_47_reg.v
@@ -0,0 +1,9 @@
+module x_200_mod_47_reg(
+    input [200:1] X,
+    output [6:1] R
+    );
+
+
+assign R = X % 47;
+
+endmodule
---
+++ modulus/mod_47/x_200/lut_6/X_34.v
@@ -0,0 +1,16 @@
+// Benchmark "X_34" written by ABC on Thu Jun 29 19:35:23 2023
+
+module X_34 ( 
+    x0, x1,
+    z0, z1, z2, z3, z4, z5  );
+  input  x0, x1;
+  output z0, z1, z2, z3, z4, z5;
+  assign z0 = x0 & x1;
+  assign z1 = ~x0 & x1;
+  assign z2 = x0 ^ x1;
+  assign z3 = x0 ? x1 : x1;
+  assign z4 = 1'b0;
+  assign z5 = x1 ? x0 : x0;
+endmodule
+
+
